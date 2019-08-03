08878e7bc288ecc570646346e9c4f31d699e5193
hoangxuyen <xuyenhv54k4cntt@gmail.com>
2019-08-03 10:17:39 +0700

    Add Domain Entities for project

---
+++ TeduShop.Model/Models/Page.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Model.Abstract;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Pages")]
+    public class Page:Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+        [Required]
+        [MaxLength(256)]
+        public string Name { set; get; }
+        [Column(TypeName ="varchar")]//Chỉ ra kiểu dữ liệu của nó
+        [MaxLength(256)]
+        [Required]
+        public string Alias { set; get; }
+        public string Content { set; get; }
+    }
+}
