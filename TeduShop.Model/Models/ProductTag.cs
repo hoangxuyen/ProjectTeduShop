b1ef7695c7d19281a8c88d0e4cbad1bae182e467
hoangxuyen <xuyenhv54k4cntt@gmail.com>
2019-08-04 10:31:49 +0700

    ok bai 5

---
+++ TeduShop.Model/Models/ProductTag.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeduShop.Model.Models
+{
+    [Table("ProductTags")]
+    internal class ProductTag
+    {
+        [Key]
+        public int ProductID { set; get; }
+
+        [Key]
+        public string TagID { set; get; }
+
+        [ForeignKey("ProductID")]
+        public virtual Product Product { set; get; }
+
+        [ForeignKey("TagID")]
+        public virtual Tag Tag { set; get; }
+    }
+}
